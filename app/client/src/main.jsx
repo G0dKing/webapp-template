e8b75bc98fa89076685913e7a0ec2f64a148329d
G0dKing <ajcaskey@gmail.com>
2024-03-22 08:22:24 -0400

    Ensured Proper Initial Deployment

--- app/client/src/main.jsx
+++ app/client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Router, Routes, Route } from 'react-router'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
